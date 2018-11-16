fd60561d5153035b09ee907fb4a2afc4820e2ce1
Marc Bejerano <marcbejerano@gmail.com>
2018-11-16 09:26:26 -0700

    Changed to using bootstrap panel

--- src/components/ForumPost/ForumPost.js
+++ src/components/ForumPost/ForumPost.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { Panel } from "react-bootstrap";
 import ReactMarkdown from "react-markdown";
 import "./ForumPost.css";
 
@@ -7,19 +8,23 @@ class ForumPost extends Component {
     const { post } = this.props;
 
     return (
-      <div>
-        <div className="post">
-          <div className="post-title">{post.title}</div>
-          <div className="post-content">
-            <ReactMarkdown source={post.content} linkTarget="_blank" />
-          </div>
-        </div>
-        <div className="reply">
-          {post.replies &&
-            post.replies.map(reply => (
-              <ForumPost key={reply.id} post={reply} />
-            ))}
-        </div>
+      <div className="post">
+        <Panel bsStyle="primary" defaultExpanded>
+          <Panel.Heading>
+            <Panel.Title toggle>{post.title}</Panel.Title>
+          </Panel.Heading>
+          <Panel.Collapse>
+            <Panel.Body>
+              <ReactMarkdown source={post.content} linkTarget="_blank" />
+            </Panel.Body>
+            <div className="reply">
+              {post.replies &&
+                post.replies.map(reply => (
+                  <ForumPost key={reply.id} post={reply} />
+                ))}
+            </div>
+          </Panel.Collapse>
+        </Panel>
       </div>
     );
   }
