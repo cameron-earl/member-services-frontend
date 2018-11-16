086f5c5f3f64c34aa529973b4664d0df79c0aef4
Marc Bejerano <marcbejerano@gmail.com>
2018-11-16 10:23:41 -0700

    Adding a reply-to button that will eventually pop up a modal ...

--- src/components/ForumPost/ForumPost.js
+++ src/components/ForumPost/ForumPost.js
@@ -3,7 +3,18 @@ import { Panel } from "react-bootstrap";
 import ReactMarkdown from "react-markdown";
 import "./ForumPost.css";
 
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faReply } from "@fortawesome/free-solid-svg-icons";
+
+library.add(faReply);
+
 class ForumPost extends Component {
+
+  reply = (post) => {
+    alert(post.title)
+  };
+
   render() {
     const { post } = this.props;
 
@@ -16,6 +27,8 @@ class ForumPost extends Component {
           <Panel.Collapse>
             <Panel.Body>
               <ReactMarkdown source={post.content} linkTarget="_blank" />
+              <div className="reply-button" onClick={()=>{this.reply(post);}}>
+                <FontAwesomeIcon icon="reply" /></div>
             </Panel.Body>
             <div className="reply">
               {post.replies &&
